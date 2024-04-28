d222b6f693c6e07193e7e96ecd18aaf31e24da9e
PedroDressler <pedro2003dressler@gmail.com>
2024-04-28 20:22:25 -0300

    solid inverse dependencie modulo finished

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import crypto from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
+import { UsersRepositories } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUserParams {
   name: string
@@ -8,25 +9,21 @@ interface RegisterUserParams {
 }
 
 export class RegisterUseCase {
-  constructor(private userRepository: any) {}
+  constructor(private userRepository: UsersRepositories) {}
 
   async handle({ name, email, password }: RegisterUserParams) {
-    const passwordHash = await crypto.hash(password, 6)
+    const password_hash = await crypto.hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('⚠️ Email already in use')
+      throw new UserAlreadyExistsError()
     }
 
     await this.userRepository.create({
       name,
       email,
-      password_hash: passwordHash,
+      password_hash,
     })
   }
 }
