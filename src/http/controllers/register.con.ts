d7f8b10839b238b28cd751816a3fa4c32475fdcd
PedroDressler <pedro2003dressler@gmail.com>
2024-05-05 18:45:38 -0300

    JWT Auth implemented

--- src/http/controllers/register.con.ts
+++ src/http/controllers/register.con.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,12 +21,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    if (err instanceof UserAlreadyExistsError) {
+    if (err instanceof ResourceNotFoundError) {
       return reply.status(409).send({ message: err.message })
     }
 
     throw err
   }
-
-  return reply.status(201).send()
 }
